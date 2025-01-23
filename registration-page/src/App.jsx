8cf7ce6b912be3fe50aec0065e8a702997207c99
YASASVIN W M Y <yasasvinyuvidu@gmail.com>
2025-01-23 16:33:05 +0530

    add

---
+++ registration-page/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import RegistrationPage from "./RegistrationPage";
+
+const App = () => {
+  return (
+    <div>
+      <RegistrationPage />
+    </div>
+  );
+};
+
+export default App;
